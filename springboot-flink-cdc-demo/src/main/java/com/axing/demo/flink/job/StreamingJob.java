0c03aa5dfb466056b0cf9197561ff541e6907608
axinger <liu_weixing@qq.com>
2022-08-22 09:45:23 +0800

    update…

--- springboot-flink-cdc-demo/src/main/java/com/axing/demo/flink/job/StreamingJob.java
+++ springboot-flink-cdc-demo/src/main/java/com/axing/demo/flink/job/StreamingJob.java
@@ -3,17 +3,15 @@ package com.axing.demo.flink.job;
 import com.alibaba.fastjson2.JSON;
 import com.axing.demo.flink.config.CheckPointConfig;
 import com.axing.demo.flink.config.MyJsonSchema;
+import com.axing.demo.flink.model.ResponseModel;
 import com.axing.demo.flink.sink.OrderSink;
 import com.axing.demo.flink.sink.UserSink;
-import com.axing.demo.flink.model.ResponseModel;
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
-import org.apache.commons.io.FileUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.typeinfo.Types;
-import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -25,13 +23,8 @@ import org.apache.flink.util.OutputTag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -39,16 +32,15 @@ import java.util.concurrent.Executors;
  * @author xing
  */
 @Component
+@Slf4j
 public class StreamingJob implements ApplicationRunner {
 
     @Autowired
     private CheckPointConfig checkPointConfig;
 
-
     ExecutorService executorService = Executors.newFixedThreadPool(1);
 
     @Override
-//    @Async
     public void run(ApplicationArguments args) {
 
         executorService.submit(() -> {
@@ -74,42 +66,14 @@ public class StreamingJob implements ApplicationRunner {
             Configuration configuration = new Configuration();
             // read checkpoint record
             // 第一次读取需要注释此行，后续增加表时，开启此行，flink-ck后 ‘27b27e36750ff997a7bd3b9933c5f3c9/chk-12404’换成存储路径下对应文件夹即可，实现旧表增量读取，新表全量读取
-            configuration.setString("execution.savepoint.path", checkPointConfig.fullPath());
+//            configuration.setString("execution.savepoint.path", checkPointConfig.fullPath());
             StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(configuration);
+            env.setRestartStrategy(RestartStrategies.noRestart());
+//            StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
             // enable checkpoint
-            env.enableCheckpointing(3000);
+//            env.enableCheckpointing(3000);
             // set local storage path
-            env.getCheckpointConfig().setCheckpointStorage(checkPointConfig.fileStorage());
-
-//            StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-//            env.registerCachedFile("/Users/xing/Desktop/flink-ck/a.txt", "distributedCache");
-//            //1：注册一个文件,可以使用hdfs上的文件 也可以是本地文件进行测试
-//            DataStreamSource<String> stringDataStreamSource = env.fromElements("Linea", "Lineb", "Linec", "Lined");
-//
-//            SingleOutputStreamOperator<String> result = stringDataStreamSource.map(new RichMapFunction<String, String>() {
-//                private ArrayList<String> dataList = new ArrayList<String>();
-//
-//                @Override
-//                public void open(Configuration parameters) throws Exception {
-//                    super.open(parameters);
-//                    //2：使用该缓存文件
-//                    File myFile = getRuntimeContext().getDistributedCache().getFile("distributedCache");
-//                    List<String> lines = FileUtils.readLines(myFile);
-//                    for (String line : lines) {
-//                        this.dataList.add(line);
-//                        System.err.println("分布式缓存为:" + line);
-//                    }
-//                }
-//
-//                @Override
-//                public String map(String value) throws Exception {
-//                    //在这里就可以使用dataList
-//                    System.err.println("使用datalist：" + dataList + "-------" + value);
-//                    //业务逻辑
-//                    return dataList + "：" + value;
-//                }
-//            });
-//            result.printToErr();
+//            env.getCheckpointConfig().setCheckpointStorage(checkPointConfig.fileStorage());
 
             // 多表进行分片处理
             OutputTag<String> orderTag = new OutputTag<>("flink_a.my_order", Types.STRING);
@@ -142,7 +106,9 @@ public class StreamingJob implements ApplicationRunner {
             userStream.addSink(new UserSink());
             env.execute("flinkCdc");
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("启动失败 = {}, = {}",e.getMessage(),e.getCause().getMessage());
+
+            System.exit(0);
         }
     }
 }
