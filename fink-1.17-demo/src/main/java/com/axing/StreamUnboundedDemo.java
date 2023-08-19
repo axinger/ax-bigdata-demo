3cf507fc79decfaacffa240d2300ae298cb34bc2
axinger <liu_weixing@qq.com>
2023-08-19 17:16:26 +0800

    update

--- fink-1.17-demo/src/main/java/com/axing/StreamUnboundedDemo.java
+++ fink-1.17-demo/src/main/java/com/axing/StreamUnboundedDemo.java
@@ -16,19 +16,26 @@ public class StreamUnboundedDemo {
         //执行环境
 
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+//        environment.setParallelism(3);//全局并行度
+
+        //本地开发,有web页面的
+//        StreamExecutionEnvironment environment = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
 
         //读取数据, socket
-        DataStreamSource<String> read = environment.socketTextStream("192.168.101.143", 7777);
+        DataStreamSource<String> read = environment.socketTextStream("hadoop102", 7777);
 
 
         //切分,转换二元组
-        SingleOutputStreamOperator<Tuple2<String, Integer>> map = read.flatMap((String value, Collector<Tuple2<String, Integer>> out) -> {
-            String[] words = value.split(" ");
-            for (String word : words) {
-                Tuple2<String, Integer> tuple2 = Tuple2.of(word, 1);
-                out.collect(tuple2);
-            }
-        }).returns(Types.TUPLE(Types.STRING, Types.INT));//lambda报错,可以在转换的算子之后调用returns(...)方法来显示指明要返回的数据类型信息。
+        SingleOutputStreamOperator<Tuple2<String, Integer>> map = read
+                .flatMap((String value, Collector<Tuple2<String, Integer>> out) -> {
+                    String[] words = value.split(" ");
+                    for (String word : words) {
+                        Tuple2<String, Integer> tuple2 = Tuple2.of(word, 1);
+                        out.collect(tuple2);
+                    }
+                })
+//                .setParallelism(3) //设置算子map的并行度
+                .returns(Types.TUPLE(Types.STRING, Types.INT));//lambda报错,可以在转换的算子之后调用returns(...)方法来显示指明要返回的数据类型信息。
 
         //按照第一个元素分组
         KeyedStream<Tuple2<String, Integer>, String> keyBy = map.keyBy((value -> value.f0));
@@ -36,7 +43,9 @@ public class StreamUnboundedDemo {
         //按照第二个元素聚合
         SingleOutputStreamOperator<Tuple2<String, Integer>> sum = keyBy.sum(1);
         //输出
-        sum.print();
+        sum.print()
+//                .setParallelism(3) //设置算子并行度
+        ;
         //流处理,执行,启动操作
         environment.execute();
 
