6c9bfee816fe2c7cb583788696a28e98fc7ad587
axinger <liu_weixing@qq.com>
2024-09-28 20:32:47 +0800

    update

--- flink-cdc-demo/src/main/java/com/github/axinger/FlinkCdc01_Test1.java
+++ flink-cdc-demo/src/main/java/com/github/axinger/FlinkCdc01_Test1.java
@@ -18,14 +18,32 @@
 
 package com.github.axinger;
 
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.TypeReference;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.state.ReducingState;
+import org.apache.flink.api.common.state.ReducingStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.cdc.connectors.mysql.source.MySqlSource;
 import org.apache.flink.cdc.connectors.mysql.table.StartupOptions;
 import org.apache.flink.cdc.debezium.JsonDebeziumDeserializationSchema;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
 public class FlinkCdc01_Test1 {
@@ -45,7 +63,7 @@ public class FlinkCdc01_Test1 {
         checkpointConfig.setMinPauseBetweenCheckpoints(2000); //两个检测点之间最小间隔
 
         // mysql8
-        Properties properties =new Properties();
+        Properties properties = new Properties();
         properties.setProperty("useSSL", "false");
         properties.setProperty("allowPublicKeyRetrieval", "true");
 
@@ -79,9 +97,69 @@ public class FlinkCdc01_Test1 {
                 .jdbcProperties(properties)
                 .build();
 
-        env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "mysql-cdc-source")
-                .print();
+        DataStreamSource<String> streamSource = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "mysql-cdc-source");
+//        streamSource.print("原始数据");
+
+        keyByAge(streamSource);
+
 
         env.execute("Print MySQL Snapshot + Binlog");
     }
+
+    private static void keyByAge(DataStreamSource<String> streamSource) {
+        streamSource
+                .map(new MapFunction<String, JSONObject>() {
+
+                    @Override
+                    public JSONObject map(String value) {
+                        System.out.println("value = " + value);
+                        JSONObject jsonObject = JSONObject.parseObject(value);
+                        return jsonObject.getJSONObject("after");
+                    }
+                })
+
+//        .process(
+//                new ProcessFunction<JSONObject,String>() {
+//
+//                    @Override
+//                    public void processElement(JSONObject s, ProcessFunction<JSONObject, String>.Context context, Collector<String> collector) throws Exception {
+//
+//                        System.out.println("s = " + s.toString(JSONWriter.Feature.WriteMapNullValue));
+////                        System.out.println("context.timestamp() = " + context.timestamp());
+//
+//                      getRuntimeContext().getReducingState(new ReducingStateDescriptor<>("lastVcStatus",
+//                                Integer::sum,
+//                                Types.INT));
+//
+//                        collector.collect(s.getString("after"));
+//                    }
+//                }
+//        )
+                .keyBy(value -> value.getString("age"))
+                .process(new KeyedProcessFunction<String, JSONObject, String>() {
+
+                    ReducingState<String> state;
+
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        super.open(parameters);
+                        // 初始化状态
+                        state = getRuntimeContext().getReducingState(new ReducingStateDescriptor<>("lastVcStatus",
+                                (value1, value2) -> StrUtil.format("{},{}", value1, value2),
+                                Types.STRING));
+
+                    }
+
+                    @Override
+                    public void processElement(JSONObject value, KeyedProcessFunction<String, JSONObject, String>.Context ctx, Collector<String> out) throws Exception {
+                        state.add(value.getString("name"));
+                        // 在后面,out
+                        out.collect(StrUtil.format("年龄为{}，的姓名有{}", value.getString("age"), state.get()));
+                    }
+
+                })
+                .print("处理后数据");
+    }
+
+
 }
